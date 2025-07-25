54221ed7b329044f3307348a20de59623e430605
Secrets Room <hamomido2490@gmail.com>
2025-07-25 18:56:48 +0300

    Create googleAds.js

---
+++ ads/googleAds.js
@@ -0,0 +1,7 @@
+export function loadGoogleAds() {
+  window.adsbygoogle = window.adsbygoogle || [];
+  window.adsbygoogle.push({
+    google_ad_client: "ca-pub-YOUR_PUB_ID",
+    enable_page_level_ads: true
+  });
+}
