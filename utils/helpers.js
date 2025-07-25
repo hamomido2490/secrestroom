f73f0e0dce0d8669faa476fcb53a502bca74f00c
Secrets Room <hamomido2490@gmail.com>
2025-07-25 19:04:03 +0300

    Create helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,18 @@
+export function formatDate(date, lang) {
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  };
+  return new Date(date).toLocaleDateString(lang === 'ar' ? 'ar-SA' : 'en-US', options);
+}
+
+export function debounce(func, wait) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
