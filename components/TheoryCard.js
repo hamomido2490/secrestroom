3bb21c2f5a257c2093fb2cdd7f3ecef9db4f0e1f
Secrets Room <hamomido2490@gmail.com>
2025-07-26 03:25:51 +0300

    Delete components directory

--- components/TheoryCard.js
+++
@@ -1,9 +0,0 @@
-export default function TheoryCard({ theory }) {
-  return 
-    <div class="theory-card" data-theory="${theory.id}">
-      <h3>${theory.name[appState.currentLanguage]}</h3>
-      <p>${theory.description[appState.currentLanguage].substring(0, 100)}...</p>
-      <button class="view-details">${translate('view_details')}</button>
-    </div>
-  ;
-}
