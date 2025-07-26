26df5fceb4a04aee119a55b21672c645613cda40
Secrets Room <hamomido2490@gmail.com>
2025-07-26 03:36:34 +0300

    Delete utils directory

--- utils/validation.js
+++
@@ -1,8 +0,0 @@
-export function validateEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-}
-
-export function validatePassword(password) {
-  return password.length >= 8;
-}
