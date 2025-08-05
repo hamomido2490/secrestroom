94c739e5cbff003c416ff7544f124fb71d7a38b1
Secrets Room <hamomido2490@gmail.com>
2025-08-05 05:17:31 +0300

    Update Chart.jsx

--- src/components/ui/Chart.jsx
+++ src/components/ui/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend } from 'chart.js';
 import { Radar } from 'react-chartjs-2';
 
