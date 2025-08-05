34091e70269a6f5b5c543357d9893c3750a3f82c
Secrets Room <hamomido2490@gmail.com>
2025-08-05 05:10:55 +0300

    Update Input.jsx

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
 
+const Input = ({ 
+  label, 
+  type = 'text', 
+  value, 
+  onChange, 
+  className = '',
+  ...props 
+}) => {
+  return (
+    <div className="mb-4">
+      {label && (
+        <label className="block mb-2 text-right">{label}</label>
+      )}
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        className={`w-full p-3 rounded-lg bg-slate-700 border border-slate-600 text-white ${className}`}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default Input;
