d4d132d4cf93af4c452dba7d04c70875ba83d8d8
Secrets Room <hamomido2490@gmail.com>
2025-08-05 04:30:23 +0300

    Select.jsx

---
+++ src/components/common/Select.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Select = ({ 
+  label, 
+  value, 
+  onChange, 
+  options = [],
+  className = '',
+  ...props 
+}) => {
+  return (
+    <div className="mb-4">
+      {label && (
+        <label className="block mb-2 text-right">{label}</label>
+      )}
+      <select
+        value={value}
+        onChange={onChange}
+        className={`w-full p-3 rounded-lg bg-slate-700 border border-slate-600 text-white ${className}`}
+        {...props}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
