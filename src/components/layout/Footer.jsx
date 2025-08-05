4420c65a3a9f5719f555674a4c0f5e04396a4172
Secrets Room <hamomido2490@gmail.com>
2025-08-05 04:29:26 +0300

    Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ({ translations, lang }) => {
+  const t = translations[lang];
+  
+  return (
+    <footer className="footer mt-16 text-center text-slate-400 text-sm">
+      <p className="font-semibold">{t.footer1}</p>
+      <p>{t.footer2}</p>
+    </footer>
+  );
+};
+
+export default Footer;
