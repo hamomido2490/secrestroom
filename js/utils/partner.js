561ac560af6d084bd426fece6e2b55ea2b60fdbc
Secrets Room <hamomido2490@gmail.com>
2025-08-06 03:58:29 +0300

    Delete js directory

--- js/utils/partner.js
+++
@@ -1,9 +0,0 @@
-function getPartnerInfo(personalityType, lang) {
-  const data = partnerInfo[lang];
-  return data[personalityType] || data['S'];
-}
-
-function getSimilarPersonalities(primary, lang) {
-  const map = { D: "I", I: "D", S: "C", C: "S" };
-  return [personalityTypes[lang][map[primary]]];
-}
